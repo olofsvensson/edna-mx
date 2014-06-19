fc67f2ec182e1b52b58eb98acf2468851668350a
Olof Svensson <svensson@esrf.fr>
2014-06-19 10:17:44 +0200

    Patch for not displaying empty forced space group in short summary

--- mxv1/plugins/EDPluginControlIndexingIndicators-v1.1/plugins/EDPluginControlIndexingIndicatorsv1_1.py
+++ mxv1/plugins/EDPluginControlIndexingIndicators-v1.1/plugins/EDPluginControlIndexingIndicatorsv1_1.py
@@ -229,7 +229,8 @@ class EDPluginControlIndexingIndicatorsv1_1(EDPluginControl):
             xsDataCrystal = self.getDataInput("crystal")[0]
             if xsDataCrystal.getSpaceGroup() is not None:
                 strForcedSpaceGroup = xsDataCrystal.getSpaceGroup().getName().getValue().upper()
-                strIndexingShortSummary += "Forced space group: %s\n" % strForcedSpaceGroup
+                if xsDataCrystal.getSpaceGroup().getName().getValue() != "":
+                    strIndexingShortSummary += "Forced space group: %s\n" % strForcedSpaceGroup
         if _xsDataIndexingResult is not None:
             # Indexing solution
             xsDataSelectedSolution = _xsDataIndexingResult.getSelectedSolution()
