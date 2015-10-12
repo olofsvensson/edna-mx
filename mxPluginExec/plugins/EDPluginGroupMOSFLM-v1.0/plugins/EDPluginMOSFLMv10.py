491d21e3a9607820c8a3cff098ea69f468297c4e
Olof Svensson <svensson@esrf.fr>
2015-10-12 11:06:34 +0200

    New version of MOSFLM: 7.2.1 of September 10th 2015

--- mxPluginExec/plugins/EDPluginGroupMOSFLM-v1.0/plugins/EDPluginMOSFLMv10.py
+++ mxPluginExec/plugins/EDPluginGroupMOSFLM-v1.0/plugins/EDPluginMOSFLMv10.py
@@ -55,6 +55,7 @@ class EDPluginMOSFLMv10(EDPluginExecProcessScript):
         self.addCompatibleVersion("Mosflm version 7.1.2  for Image plate and CCD data 16th June 2014")
         self.addCompatibleVersion("Mosflm version 7.1.3  for Image plate and CCD data 17th February 2015")
         self.addCompatibleVersion("Mosflm version 7.2.0  for IP, CCD and PAD data 5th June 2015")
+        self.addCompatibleVersion("Mosflm version 7.2.1  for IP, CCD and PAD data 10th September 2015")
 
         self.strMOSFLMNewmatFileName = None
         self.strMOSFLMMatrixFileName = None
