9b7810ace9cc8dd67571090223523b8e58084b7b
Olof Svensson <svensson@esrf.fr>
2016-01-08 14:20:13 +0100

    New Eiger 4m test cases

---
+++ mxPluginExec/plugins/EDPluginGroupPhenix-v1.1/tests/testsuite/EDTestCasePluginExecuteDistlSignalStrengthv1_1_eiger4m.py
@@ -0,0 +1,64 @@
+#
+#    Project: mxPluginExec
+#             http://www.edna-site.org
+#
+#    Copyright (C) 2008-2012 European Synchrotron Radiation Facility
+#                            Grenoble, France
+#
+#    Principal authors:      Olof Svensson (svensson@esrf.fr)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    and the GNU Lesser General Public License  along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+
+__authors__ = [ "Olof Svensson" ]
+__contact__ = "svensson@esrf.fr"
+__license__ = "LGPLv3+"
+__copyright__ = "European Synchrotron Radiation Facility, Grenoble, France"
+__date__ = "20120712"
+__status__ = "production"
+
+
+import os
+
+from EDTestCasePluginExecute import EDTestCasePluginExecute
+
+
+
+class EDTestCasePluginExecuteDistlSignalStrengthv1_1_eiger4m(EDTestCasePluginExecute):
+
+    def __init__(self, _edStringTestName=None):
+        EDTestCasePluginExecute.__init__(self, "EDPluginDistlSignalStrengthv1_1")
+        self.setConfigurationFile(self.getRefConfigFile())
+        self.setDataInputFile(os.path.join(self.getPluginTestsDataHome(), "XSDataInputDistlSignalStrength_eiger4m.xml"))
+#        self.setReferenceDataOutputFile(os.path.join(self.getPluginTestsDataHome(), "XSDataResultDistlSignalStrength_reference.xml"))
+
+    def preProcess(self):
+        EDTestCasePluginExecute.preProcess(self)
+        self.loadTestImage([ "lyso_12_0001.cbf" ])
+
+
+    def testExecute(self):
+        self.run()
+
+
+    def process(self):
+        self.addTestMethod(self.testExecute)
+
+
+
+if __name__ == '__main__':
+
+    edTestCasePluginExecuteDistlSignalStrengthv1_1 = EDTestCasePluginExecuteDistlSignalStrengthv1_1("EDTestCasePluginExecuteDistlSignalStrengthv1_1")
+    edTestCasePluginExecuteDistlSignalStrengthv1_1.execute()
