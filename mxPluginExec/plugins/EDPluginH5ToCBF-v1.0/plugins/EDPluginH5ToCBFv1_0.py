82a6400a58cacebb9d2b75d9ff1623f7e7a4609d
Olof Svensson <svensson@esrf.fr>
2016-01-11 17:00:12 +0100

    Updated for the latest version of H5ToXds

--- mxPluginExec/plugins/EDPluginH5ToCBF-v1.0/plugins/EDPluginH5ToCBFv1_0.py
+++ mxPluginExec/plugins/EDPluginH5ToCBF-v1.0/plugins/EDPluginH5ToCBFv1_0.py
@@ -40,7 +40,7 @@ class EDPluginH5ToCBFv1_0(EDPluginExecProcessScript):
     """
     This plugin runs H5ToXDS to create CBF files from an Eiger HDF5 file. 
     """
-    
+
 
     def __init__(self):
         EDPluginExecProcessScript.__init__(self)
@@ -59,15 +59,15 @@ class EDPluginH5ToCBFv1_0(EDPluginExecProcessScript):
         self.checkMandatoryParameters(self.dataInput.hdf5File, "HDF5 file is None")
         self.checkMandatoryParameters(self.dataInput.dataCollection, "Data collection is None")
 
-    
+
     def preProcess(self, _edObject=None):
         EDPluginExecProcessScript.preProcess(self)
         self.DEBUG("EDPluginH5ToCBFv1_0.preProcess")
         xsDataInputH5ToCBF = self.getDataInput()
         self.setScriptCommandline(self.generateCommands(xsDataInputH5ToCBF))
 
-        
-        
+
+
     def process(self, _edObject=None):
         EDPluginExecProcessScript.process(self)
         self.DEBUG("EDPluginH5ToCBFv1_0.process")
@@ -79,14 +79,14 @@ class EDPluginH5ToCBFv1_0(EDPluginExecProcessScript):
         fileTmpCBF = open(self.tmpCBFFile)
         tmpCBF = fileTmpCBF.read()
         fileTmpCBF.close()
-        
+
         # Replace opening line
         tmpCBF = tmpCBF.replace("CBF: VERSION 1.5, CBFlib v0.7.8 - SLS/DECTRIS PILATUS detectors",
                                 "CBF: VERSION 1.5, CBFlib v0.7.8")
-        
-        
+
+
         index1 = tmpCBF.find("# WARNING: FOR XDS PROCESSING ONLY.")
-        string2 = "# SOFTWARE VERSION: 1.0"
+        string2 = "# SOFTWARE VERSION: 1.1.0-RELEASE"
         index2 = tmpCBF.find(string2) + len(string2)
 
         miniCBFHeader = self.generateMiniCBFHeader(self.dataInput)
@@ -95,9 +95,9 @@ class EDPluginH5ToCBFv1_0(EDPluginExecProcessScript):
         newCBFFile = open(self.CBFFile, "w")
         newCBFFile.write(newCBF)
         newCBFFile.close()
-    
-        
-        
+
+
+
     def finallyProcess(self, _edObject=None):
         EDPluginExecProcessScript.finallyProcess(self)
         self.DEBUG("EDPluginH5ToCBFv1_0.finallyProcess")
@@ -108,13 +108,13 @@ class EDPluginH5ToCBFv1_0(EDPluginExecProcessScript):
             if os.path.exists(self.CBFFile):
                 self.dataOutput.outputCBFFile = XSDataFile(XSDataString(self.CBFFile))
 
-    
+
     def generateCommands(self, _xsDataInputH5ToCBF):
         """
         This method creates a list of commands for H5ToXDS
         """
         self.DEBUG("EDPluginH5ToCBFv1_0.generateCommands")
-        
+
         hdf5File = _xsDataInputH5ToCBF.hdf5File.path.value
         directory = os.path.dirname(hdf5File)
         prefix = EDUtilsImage.getPrefix(hdf5File)
@@ -122,53 +122,53 @@ class EDPluginH5ToCBFv1_0(EDPluginExecProcessScript):
             masterFile = hdf5File
         else:
             masterFile = os.path.join(directory, prefix + "__master.h5")
-        
+
         imageNumber = _xsDataInputH5ToCBF.imageNumber.value
-        
+
         CBFFileName = prefix + "_%04d" % imageNumber + ".cbf"
         tmpCBFFileName = "tmp_" + CBFFileName
-        
+
         self.CBFFile = os.path.join(directory, CBFFileName)
         self.tmpCBFFile = os.path.join(self.getWorkingDirectory(), tmpCBFFileName)
-        
+
         scriptCommandLine = "{0} {1} {2}".format(masterFile, imageNumber, tmpCBFFileName)
-        
+
         return scriptCommandLine
 
     def generateMiniCBFHeader(self, _xsDataInputH5ToCBF):
         dataCollection = _xsDataInputH5ToCBF.dataCollection
         miniCBFHeader = ""
         miniCBFHeader += "# Detector: Dectris Eiger 4M, S/N E-08-0106, ESRF ID30a3\r\n"
-        miniCBFHeader += "# {0}\r\n".format(dataCollection.startTime) 
-        miniCBFHeader += "# Pixel_size 75e-6 m x 75e-6 m\r\n" 
-        miniCBFHeader += "# Silicon sensor, thickness 0.000320 m\r\n" 
-        miniCBFHeader += "# Oscillation_axis omega\r\n" 
-        miniCBFHeader += "# Chi 0.0000 deg\r\n" 
+        miniCBFHeader += "# {0}\r\n".format(dataCollection.startTime)
+        miniCBFHeader += "# Pixel_size 75e-6 m x 75e-6 m\r\n"
+        miniCBFHeader += "# Silicon sensor, thickness 0.000320 m\r\n"
+        miniCBFHeader += "# Oscillation_axis omega\r\n"
+        miniCBFHeader += "# Chi 0.0000 deg\r\n"
         miniCBFHeader += "# Angle_increment {0:.4f} deg.\r\n".format(dataCollection.axisRange)
-        miniCBFHeader += "# Polarization 0.99\r\n" 
-        miniCBFHeader += "# file_comments\r\n" 
-        miniCBFHeader += "# N_oscillations 1\r\n" 
+        miniCBFHeader += "# Polarization 0.99\r\n"
+        miniCBFHeader += "# file_comments\r\n"
+        miniCBFHeader += "# N_oscillations 1\r\n"
         xbeam = dataCollection.xbeam
         ybeam = dataCollection.ybeam
-        miniCBFHeader += "# Beam_xy ({0:.2f}, {1:.2f}) pixels\r\n".format(xbeam/0.075, ybeam/0.075)
+        miniCBFHeader += "# Beam_xy ({0:.2f}, {1:.2f}) pixels\r\n".format(xbeam / 0.075, ybeam / 0.075)
         miniCBFHeader += "# Exposure_time {0:.6f} s\r\n".format(dataCollection.exposureTime)
-        miniCBFHeader += "# Phi 0.0020 deg.\r\n" 
-        miniCBFHeader += "# Energy_range (0, 0) eV\r\n" 
+        miniCBFHeader += "# Phi 0.0020 deg.\r\n"
+        miniCBFHeader += "# Energy_range (0, 0) eV\r\n"
         miniCBFHeader += "# Start_angle {0:.4f} deg.\r\n".format(dataCollection.axisStart)
-        miniCBFHeader += "# Detector_distance {0:.6f} m\r\n".format(dataCollection.detectorDistance/1000.0) 
-        miniCBFHeader += "# Detector_Voffset 0.0000 m\r\n" 
-        miniCBFHeader += "# Alpha 0.0000 deg.\r\n" 
-        miniCBFHeader += "# Flat_field: (nil)\r\n" 
-        miniCBFHeader += "# Threshold_setting 5980 eV\r\n" 
-        miniCBFHeader += "# Exposure_period 0.020950 s\r\n" 
-        miniCBFHeader += "# N_excluded_pixels: = 321\r\n" 
-        miniCBFHeader += "# Kappa 0.0020 deg.\r\n" 
-        miniCBFHeader += "# Tau = 0 s\r\n" 
-        miniCBFHeader += "# Transmission {0:.2f}\r\n".format(dataCollection.transmission) 
-        miniCBFHeader += "# Detector_2theta 0.0000 deg.\r\n" 
+        miniCBFHeader += "# Detector_distance {0:.6f} m\r\n".format(dataCollection.detectorDistance / 1000.0)
+        miniCBFHeader += "# Detector_Voffset 0.0000 m\r\n"
+        miniCBFHeader += "# Alpha 0.0000 deg.\r\n"
+        miniCBFHeader += "# Flat_field: (nil)\r\n"
+        miniCBFHeader += "# Threshold_setting 5980 eV\r\n"
+        miniCBFHeader += "# Exposure_period 0.020950 s\r\n"
+        miniCBFHeader += "# N_excluded_pixels: = 321\r\n"
+        miniCBFHeader += "# Kappa 0.0020 deg.\r\n"
+        miniCBFHeader += "# Tau = 0 s\r\n"
+        miniCBFHeader += "# Transmission {0:.2f}\r\n".format(dataCollection.transmission)
+        miniCBFHeader += "# Detector_2theta 0.0000 deg.\r\n"
         miniCBFHeader += "# Flux {0:.1f}\r\n".format(dataCollection.flux)
-        miniCBFHeader += "# Count_cutoff 1048500\r\n" 
-        miniCBFHeader += "# Trim_directory: (nil)\r\n" 
+        miniCBFHeader += "# Count_cutoff 1048500\r\n"
+        miniCBFHeader += "# Trim_directory: (nil)\r\n"
         miniCBFHeader += "# Wavelength {0:.6f} A".format(dataCollection.wavelength)
         return miniCBFHeader
 
@@ -181,7 +181,7 @@ class EDPluginH5ToCBFv1_0(EDPluginExecProcessScript):
 # Chi 0.0000 deg.
 # Angle_increment 0.1000 deg.
 # Polarization 0.99
-# file_comments 
+# file_comments
 # N_oscillations 1
 # Beam_xy (1227.22, 1255.72) pixels
 # Exposure_time 0.020000 s
@@ -203,4 +203,4 @@ class EDPluginH5ToCBFv1_0(EDPluginExecProcessScript):
 # Count_cutoff 1048500
 # Trim_directory: (nil)
 # Wavelength 1.243944 A
-            
+
