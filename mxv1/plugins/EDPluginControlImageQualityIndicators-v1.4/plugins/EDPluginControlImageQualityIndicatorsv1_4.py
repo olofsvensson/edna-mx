a28f3e02ed992993bdc7b1aaa790bb1abc8fd624
Olof Svensson <svensson@esrf.fr>
2016-06-08 14:05:50 +0200

    Fixed problem if shape is of length 1

--- mxv1/plugins/EDPluginControlImageQualityIndicators-v1.4/plugins/EDPluginControlImageQualityIndicatorsv1_4.py
+++ mxv1/plugins/EDPluginControlImageQualityIndicators-v1.4/plugins/EDPluginControlImageQualityIndicatorsv1_4.py
@@ -286,7 +286,8 @@ class EDPluginControlImageQualityIndicatorsv1_4(EDPluginControl):
                                 numpyArray = numpy.loadtxt(imageDozor.spotFile.path.value, skiprows=3)
                                 xsDataImageQualityIndicators.dozorSpotList = XSDataString(base64.b64encode(numpyArray.tostring()))
                                 xsDataImageQualityIndicators.addDozorSpotListShape(XSDataInteger(numpyArray.shape[0]))
-                                xsDataImageQualityIndicators.addDozorSpotListShape(XSDataInteger(numpyArray.shape[1]))
+                                if len(numpyArray.shape) > 1:
+                                    xsDataImageQualityIndicators.addDozorSpotListShape(XSDataInteger(numpyArray.shape[1]))
                         xsDataImageQualityIndicators.dozorSpotsIntAver = imageDozor.spots_int_aver
                         xsDataImageQualityIndicators.dozorSpotsResolution = imageDozor.spots_resolution
                         if self.xsDataResultControlImageQualityIndicators.inputDozor is None:
