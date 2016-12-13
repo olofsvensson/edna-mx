ff765b0587b6a0531c8cca4a68c591ede5dee621
Olof Svensson <svensson@esrf.fr>
2016-12-13 14:54:49 +0100

    Change of colours, font size for tables

--- mxv1/plugins/EDPluginExecSimpleHTMLPage-v1.1/plugins/report.py
+++ mxv1/plugins/EDPluginExecSimpleHTMLPage-v1.1/plugins/report.py
@@ -164,31 +164,34 @@ class WorkflowStepReport(object):
                 page.table.close()
                 page.br()
             elif item["type"] == "table":
+                titleBgColour = "#99c2ff"
+                columnTitleBgColour = "#ffffff"
+                dataBgColour = "#e6f0ff"
                 page.table(border_="1",
                            cellpadding_="2",
                            width="100%",
-                           style_="border-collapse: collapse; margin: 0px;")
+                           style_="border-collapse: collapse; margin: 0px; font-size: 12px")
                 page.tr(align_="LEFT")
                 # page.strong(itemTitle)
-                page.th(itemTitle, bgcolor_="#F0F0FF", align_="LEFT", style_="padding: 5px;", colspan_=str(len(item["columns"])))
+                page.th(itemTitle, bgcolor_=titleBgColour, align_="LEFT", style_="padding: 5px;", colspan_=str(len(item["columns"])))
                 page.tr.close()
                 if "orientation" in item and item["orientation"] == "vertical":
                     for index1 in range(len(item["columns"])):
                         itemColumn = self.escapeCharacters(item["columns"][index1])
-                        page.tr(align_="CENTER")
-                        page.th(itemColumn, bgcolor_="#F0F0FF", align_="LEFT", style_="padding: 5px;")
+                        page.tr(align_="LEFT")
+                        page.th(itemColumn, bgcolor_=columnTitleBgColour, align_="LEFT", style_="padding: 5px;")
                         for index2 in range(len(item["data"])):
                             itemData = self.escapeCharacters(str(item["data"][index2][index1]))
-                            page.th(itemData, bgcolor_="#FFFFA0", style_="padding: 5px;")
+                            page.th(itemData, bgcolor_=dataBgColour, style_="padding: 5px;")
                         page.tr.close()
                 else:
-                    page.tr(align_="CENTER", bgcolor_="#F0F0FF", style_="padding: 5px;")
+                    page.tr(align_="LEFT", bgcolor_=columnTitleBgColour, style_="padding: 5px;")
                     for column in item["columns"]:
                         itemColumn = self.escapeCharacters(column)
                         page.th(itemColumn, style_="padding: 5px;")
                     page.tr.close()
                     for listRow in item["data"]:
-                        page.tr(align_="CENTER", bgcolor_="#FFFFA0", style_="padding: 5px;")
+                        page.tr(align_="LEFT", bgcolor_=dataBgColour, style_="padding: 5px;")
                         for cell in listRow:
                             itemCell = self.escapeCharacters(str(cell))
                             page.th(itemCell, style_="padding: 5px;")
