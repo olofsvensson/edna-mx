35790097c3e76d9c3c31b7615b47a23f5f2c3de5
Olof Svensson <svensson@esrf.fr>
2017-04-04 14:51:44 +0200

    Replaced bm14 with bm30a

--- mxv1/src/EDHandlerESRFPyarchv1_0.py
+++ mxv1/src/EDHandlerESRFPyarchv1_0.py
@@ -59,7 +59,7 @@ class EDHandlerESRFPyarchv1_0:
             return strPyarchDNAFilePath
 
 
-        listBeamlines = ["bm14", "id14eh1", "id14eh2", "id14eh3", "id14eh4", "id23eh1", "id23eh2",
+        listBeamlines = ["bm30a", "id14eh1", "id14eh2", "id14eh3", "id14eh4", "id23eh1", "id23eh2",
                          "id29", "id30a1", "id30a2", "id30a3", "id30b", "simulator_mxcube"]
         # Check that we have at least four levels of directories:
         if (len(listOfDirectories) > 5):
