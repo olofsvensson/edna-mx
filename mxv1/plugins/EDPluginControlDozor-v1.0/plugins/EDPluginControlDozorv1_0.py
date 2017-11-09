4761fe41ca46df11be8945728bdfe2cadfcd26ca
Olof Svensson <svensson@esrf.fr>
2017-11-09 15:09:56 +0100

    Now keeps CBF images in raw data directory

--- mxv1/plugins/EDPluginControlDozor-v1.0/plugins/EDPluginControlDozorv1_0.py
+++ mxv1/plugins/EDPluginControlDozor-v1.0/plugins/EDPluginControlDozorv1_0.py
@@ -94,6 +94,7 @@ class EDPluginControlDozorv1_0(EDPluginControl):
         self.hdf5BatchSize = None
         self._strMxCuBE_URI = None
         self._oServerProxy = None
+        self.doRadiationDamage = False
 
 
     def checkParameters(self):
@@ -120,6 +121,8 @@ class EDPluginControlDozorv1_0(EDPluginControl):
         self.edPluginControlReadImageHeader = self.loadPlugin(self.strEDPluginControlReadImageHeaderName, "SubWedgeAssemble")
         if self.dataInput.batchSize is not None:
             self.batchSize = self.dataInput.batchSize.value
+        if self.dataInput.radiationDamage is not None:
+            self.doRadiationDamage = self.dataInput.radiationDamage.value
 
 
 
@@ -162,9 +165,12 @@ class EDPluginControlDozorv1_0(EDPluginControl):
         if dictImage[listAllBatches[0][0]].path.value.endswith("h5"):
             # Convert HDF5 images to CBF
             self.screen("HDF5 converter batch size: {0}".format(self.batchSize))
-            self.cbfTempDir = tempfile.mkdtemp(prefix="CbfTemp_")
+            if self.doRadiationDamage:
+                self.cbfTempDir = None
+            else:
+                self.cbfTempDir = tempfile.mkdtemp(prefix="CbfTemp_")
             listHdf5Batches = self.createListOfBatches(dictImage.keys(), self.batchSize)
-            dictImage, self.hasHdf5Prefix = self.convertToCBF(dictImage, listHdf5Batches, self.dataInput.radiationDamage)
+            dictImage, self.hasHdf5Prefix = self.convertToCBF(dictImage, listHdf5Batches, self.doRadiationDamage)
         for listBatch in listAllBatches:
             # Read the header from the first image in the batch
             xsDataFile = dictImage[listBatch[0]]
@@ -532,7 +538,7 @@ class EDPluginControlDozorv1_0(EDPluginControl):
             listAllBatches.append(listImagesInBatch)
         return listAllBatches
 
-    def convertToCBF(self, dictImage, listAllBatches, radiationDamage=False):
+    def convertToCBF(self, dictImage, listAllBatches, doRadiationDamage=False):
         # Find start and end image number
         startImage = None
         endImage = None
@@ -559,9 +565,10 @@ class EDPluginControlDozorv1_0(EDPluginControl):
                 newDict[image] = edPluginH5ToCBF.dataOutput.outputCBFFile
         else:
             listPluginH5ToCBF = []
+            directory = os.path.dirname(dictImage[listAllBatches[0][0]].path.value)
             for batch in listAllBatches:
                 xsDataInputH5ToCBF = XSDataInputH5ToCBF()
-                if radiationDamage:
+                if doRadiationDamage:
                     xsDataInputH5ToCBF.hdf5File = dictImage[batch[0]]
                     xsDataInputH5ToCBF.hdf5ImageNumber = XSDataInteger(batch[0])
                     xsDataInputH5ToCBF.startImageNumber = XSDataInteger(listAllBatches[0][0])
@@ -571,11 +578,10 @@ class EDPluginControlDozorv1_0(EDPluginControl):
                     xsDataInputH5ToCBF.hdf5ImageNumber = XSDataInteger(1)
                     xsDataInputH5ToCBF.startImageNumber = XSDataInteger(batch[0])
                     xsDataInputH5ToCBF.endImageNumber = XSDataInteger(batch[-1])
-                xsDataInputH5ToCBF.forcedOutputDirectory = XSDataFile(XSDataString(self.cbfTempDir))
+                    xsDataInputH5ToCBF.forcedOutputDirectory = XSDataFile(XSDataString(self.cbfTempDir))
                 edPluginH5ToCBF = self.loadPlugin("EDPluginH5ToCBFv1_1")
                 edPluginH5ToCBF.dataInput = xsDataInputH5ToCBF
-                if radiationDamage:
-                    directory = os.path.dirname(dictImage[batch[0]].path.value)
+                if doRadiationDamage:
                     edPluginH5ToCBF.executeSynchronous()
                     if edPluginH5ToCBF.dataOutput is not None and edPluginH5ToCBF.dataOutput.outputCBFFileTemplate is not None:
                         outputCBFFileTemplate = edPluginH5ToCBF.dataOutput.outputCBFFileTemplate
